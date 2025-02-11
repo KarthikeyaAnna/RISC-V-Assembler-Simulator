1299bb2ed7b57727c2782479f925b5c7cc633630
Shreyas-180 <shreyas24544@iiitd.ac.in>
2025-02-11 13:52:23 +0530

    check if handeled

--- Assembler.py
+++ Assembler.py
@@ -155,21 +155,40 @@ with open("input.txt",'r') as file:
        
         
         if(instruction in data["INSTRUCTION_FORMATS"]["S"]):
-            split_value=list_line_sep[2].strip('()').split('(')
-            list_line_sep.pop(2)
-            list_line_sep.insert(2,split_value[1]) 
-            list_line_sep.insert(2,split_value[0])
-            if(len(list_line_sep)!=4):  
-                binary_output="ERROR"
-                
-            r2=list_line_sep[1]
-            r1=list_line_sep[3]
-            netimm=convert(list_line_sep[2])
-            imm_11_5=netimm[:7]
-            imm_4_0=netimm[7:]
-            binary_output+=imm_11_5+data["REGISTER_MAP"][r2]+data["REGISTER_MAP"][r1]+data["FUNCT3"][instruction]+imm_4_0+data["OPCODES"][instruction]
-            if int(netimm)>2**12:
-                binary_output="Error" 
+            if (len(list_line_sep)>4 or len(list_line_sep)<3):
+                binary_output="ERROR: Invalid Format"
+            else:
+                if len(list_line_sep)==3:
+                    split_value=list_line_sep[2].strip('()').split('(')
+                    if len(split_value)!=2:
+                        binary_output="ERROR: Invalid Format"
+                    
+                    else:
+                        list_line_sep.pop(2)
+                        list_line_sep.insert(2,split_value[1]) 
+                        list_line_sep.insert(2,split_value[0])
+                        r2=list_line_sep[1]
+                        r1=list_line_sep[3]
+                        netimm=convert(list_line_sep[2])
+                        imm_11_5=netimm[:7]
+                        imm_4_0=netimm[7:]
+                        binary_output+=imm_11_5+data["REGISTER_MAP"][r2]+data["REGISTER_MAP"][r1]+data["FUNCT3"][instruction]+imm_4_0+data["OPCODES"][instruction]
+                        if int(netimm)>2**12:
+                            binary_output="Error"
+                   
+                elif len(list_line_sep)==4: 
+                    split_value=list_line_sep[2].strip('()').split('(')
+                    list_line_sep.pop(2)
+                    list_line_sep.insert(2,split_value[1]) 
+                    list_line_sep.insert(2,split_value[0])
+                    r2=list_line_sep[1]
+                    r1=list_line_sep[3]
+                    netimm=convert(list_line_sep[2])
+                    imm_11_5=netimm[:7]
+                    imm_4_0=netimm[7:]
+                    binary_output+=imm_11_5+data["REGISTER_MAP"][r2]+data["REGISTER_MAP"][r1]+data["FUNCT3"][instruction]+imm_4_0+data["OPCODES"][instruction]
+                    if int(netimm)>2**12:
+                        binary_output="Error"
 
 
         
